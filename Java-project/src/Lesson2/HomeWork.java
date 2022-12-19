7ab71d049cbe654537197dc0f5e2f3c52bbc376d
SergeyStrechen <sergey.stre4en@gmail.com>
2022-12-20 00:12:30 +0200

    HomeWork

---
+++ Java-project/src/Lesson2/HomeWork.java
@@ -0,0 +1,19 @@
+package Lesson2;
+
+public class HomeWork {
+    public static void main(String[] args) {
+        byte a = 5, b = 8;
+        short c = 7, d = 9;
+        int e = 11, f = 13;
+        long h = 15, i = 17;
+        System.out.printf("(%s %s) (%s %s) (%s %s) (%s %s)\n", a,b,c,d,e,f,h,i);
+        float j = 19;
+        double k = 21.1;
+        boolean l = false;
+        System.out.printf("%s %s %s\n",j,k,l);
+        char m = 'n';
+        char p = '\uf54e';
+        System.out.printf("%s %s\n",m,p);
+
+    }
+}
