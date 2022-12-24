d57c6bfc0af2f2ce843e8a33a96ac2528aa3629f
SergeyStrechen <sergey.stre4en@gmail.com>
2022-12-24 16:52:27 +0200

    Lesson_03

--- Java-project/src/Lesson2/HomeTask/HomeTask.java
+++ Java-project/src/Lesson2/HomeTask/HomeTask.java
@@ -5,7 +5,7 @@ public class HomeTask {
         byte a = 5, b = 8;
         short c = 7, d = 9;
         int e = 11, f = 13;
-        long h = 15234L, i = 17234L;
+        long h = 15234L, i = 17234;
         System.out.printf("(%s %s) (%s %s) (%s %s) (%s %s)\n", a,b,c,d,e,f,h,i);
         float j = 19234F, q = 20343242F;
         double k = 21.1, w = 23.3;
